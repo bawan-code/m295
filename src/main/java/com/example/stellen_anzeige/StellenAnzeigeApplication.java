490aabf7564a9bd5594eb83e2b48ed0315af8acb
bawan <bawan.mahmud@bbzbl-it.ch>
2026-05-04 09:16:04 +0200

    Init SpringBoot

---
+++ src/main/java/com/example/stellen_anzeige/StellenAnzeigeApplication.java
@@ -0,0 +1,13 @@
+package com.example.stellen_anzeige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellenAnzeigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellenAnzeigeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stellen_anzeige/StellenAnzeigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stellen_anzeige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellenAnzeigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
