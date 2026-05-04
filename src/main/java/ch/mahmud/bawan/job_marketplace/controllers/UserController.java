653080fcb1bb3d3b39b4467b01464eedbfcd2498
bawan <bawan.mahmud@bbzbl-it.ch>
2026-05-04 16:10:01 +0200

    Anfang Controllers (User controller Get ALL Methode)

--- src/main/java/ch/mahmud/bawan/job_marketplace/controllers/UserController.java
+++ src/main/java/ch/mahmud/bawan/job_marketplace/controllers/UserController.java
@@ -1,4 +1,34 @@
 package ch.mahmud.bawan.job_marketplace.controllers;
 
+import ch.mahmud.bawan.job_marketplace.models.User;
+import ch.mahmud.bawan.job_marketplace.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Validated
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Tag(name = "User", description = "Get users")
+    @Operation(summary = "Get all users", description = "Returns a list of all users")
+    @GetMapping
+    public ResponseEntity<List<User>> all() {
+        List<User> result = userService.getAllUsers();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
